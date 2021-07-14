3f677614f09fc62cd629932d3ecb86c6f39a699b
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-14 15:33:36 -0300

    chore: final initial configuration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({ test: 200 })
+  res.json({ status: 200 })
 })
 
-app.listen(5000, () => {
-    console.log('server is running.')
-})
-
-
+app.listen(5000, () => console.log('Server is running'))
