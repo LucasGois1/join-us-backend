f222db4c0a859d53a012610b6698a1f8ee1f6e0c
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-14 21:43:13 -0300

    feat: ensure signUpController return an error if no parameters is provided

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,8 +1,28 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
+    if (!httpRequest.body.password) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: password')
+      }
+    }
+    if (!httpRequest.body.confirmPassword) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: confirmPassword')
+      }
     }
   }
 }
