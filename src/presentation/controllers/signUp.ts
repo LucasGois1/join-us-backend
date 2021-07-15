ea4acb7adc6b0d04ecff7a4d6593872d24c6aa3b
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-14 22:20:06 -0300

    refactor: simplify code

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,19 +4,11 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { name, email, password, confirmPassword } = httpRequest.body
-
-    if (!name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
-    }
-    if (!password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    if (!confirmPassword) {
-      return badRequest(new MissingParamError('passwordConfirmation'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
