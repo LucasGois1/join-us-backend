e5ed2133a4e1c394335984dd6f868cf91bf803db
Lucas Gois <lucas.gois@linnks.com.br>
2021-07-18 14:45:45 -0300

    feat: ensure LoginCOntroller throws if email Validator throws

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,35 @@
+import { EmailValidator } from './../../protocols/email-validator'
+import { MissingParamError } from './../../errors/missing-param-error'
+import { badRequest, serverError, success } from './../../helper/http-helper'
+import { HttpRequest, HttpResponse } from './../../protocols/http'
+import { Controller } from '../../protocols'
+import { InvalidParamError } from '../../errors'
+
+export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const { email, password } = httpRequest.body
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      return new Promise(resolve => resolve(success({})))
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
