45c7c6b0188af264e03454299405f6d30830c722
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-18 18:54:59 -0300

    ensure logDecorator calls controler methods

--- src/main/factorys/signup.ts
+++ src/main/factorys/signup.ts
@@ -3,11 +3,14 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
   const encrypter = new BcryptAdapter()
   const mongoAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, mongoAccountRepository)
-  return new SignUpController(emailValidator, addAccount)
+  const signUpController = new SignUpController(emailValidator, addAccount)
+  return new LogControllerDecorator(signUpController)
 }
