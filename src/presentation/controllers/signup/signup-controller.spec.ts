0c89c65c0a5c178d80d6a54f6e8734b68c0e030f
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-20 12:29:25 -0300

    feat: ensure signUpController returns 400 if validation fails

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -212,4 +212,12 @@ describe('Sign up controller suite', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('should return 400 if validator returns an error',async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
