49e34fc1d91e90cd7c7bc1a2c152fbe1cebd4924
LucasGois1 <lucas.gois@linnks.com.br>
2021-07-19 23:01:47 -0300

    feat: ensure log mongo repository save log errors

--- src/infra/db/mongodb/log-error-repository/log-error.ts
+++
@@ -1,7 +0,0 @@
-import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
-
-export class LogError implements LogErrorRepository {
-  async log (stack: string): Promise<void> {
-    return new Promise(resolve => resolve())
-  }
-}
