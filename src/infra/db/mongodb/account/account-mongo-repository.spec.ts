f1b86c775a5e82ef7e98deecc7ba9b6e472ccde0
LucasGois1 <lucas.gois@linnks.com.br>
2021-08-10 15:18:12 -0300

    refactor: change folders name

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -1,6 +1,6 @@
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { AccountMongoRepository } from './account'
+import { AccountMongoRepository } from './account-mongo-repository'
 
 const makeFakeAddAccount = (): AddAccountModel => ({
   name: 'any_name',
