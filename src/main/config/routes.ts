1262d3d2af7c4723e5a62135f80973521d4c7504
Lucas Gois <lucas.gois@linnks.com.br>
2021-09-11 18:19:01 -0300

    chore & refactor: up depend & change route imports

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,14 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
+  // eslint-disable-next-line node/no-path-concat
+  readdirSync(join(__dirname,'../routes'))
+    .filter(file => !file.endsWith('.map'))
+    .map(async file => {
+      (await import(`../routes/${file}`)).default(router)
+    })
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => { (await import(`../../../${file}`)).default(router) })
 }
