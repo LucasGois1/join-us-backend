7e91a949e7b22c983399697a33fe8d2343f46f37
Lucas Gois <lucas.gois@linnks.com.br>
2022-02-23 16:39:17 -0300

    fix: port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://database:27017/join-us-database',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/join-us-database',
   mongoUrlATLAS: process.env.MONGO_URL || 'mongodb+srv://lucasgoisdev:LqlG1egm19Fc0xhW@joinus.l7okp.mongodb.net/JoinUS?retryWrites=true&w=majority',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'jks79812bjkb8¨55615vb¨%!'
